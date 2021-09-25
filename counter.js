5c17488dd419b12988393f38ef1639c5d5741545
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-09-25 15:55:55 +0900

    feat: 0の時文字が表示される機能を追加しました

--- counter.js
+++ counter.js
@@ -1,32 +1,46 @@
-const $counter = document.getElementById('counter'); 
-const $up = document.getElementById('count-up'); 
-const $down = document.getElementById('count-down'); 
-const $tenUp = document.getElementById('ten-up'); 
-const $tenDown = document.getElementById('ten-down'); 
-const $reset = document.getElementById('reset'); 
+const $counter = document.getElementById('counter');
+const $up = document.getElementById('count-up');
+const $down = document.getElementById('count-down');
+const $tenUp = document.getElementById('ten-up');
+const $tenDown = document.getElementById('ten-down');
+const $reset = document.getElementById('reset');
 let count = 0;
 
 $up.addEventListener('click', function () {
   count = count + 1;
   counter.innerHTML = count;
+  string();
 });
 
 $tenUp.addEventListener('click', function () {
   count = count + 10;
   counter.innerHTML = count;
+  string();
 });
 
 $down.addEventListener('click', function () {
   count = count - 1;
   counter.innerHTML = count;
+  string();
 });
 
 $tenDown.addEventListener('click', function () {
   count = count - 10;
   counter.innerHTML = count;
+  string();
 });
 
 $reset.addEventListener('click', function () {
   count = 0;
   counter.innerHTML = count;
+  string();
 });
+
+const stringArea = document.getElementById('stringArea');
+const string = () => {
+  if(count == 0){
+    stringArea.innerHTML = "0なんですけど";
+  }else{
+    stringArea.innerHTML = "  ";
+  };
+};

19e58cbf7315fb6acace370faaab2abbe27b5f9f
kahca-122 <kacha_kacha_katchan@icloud.com>
2021-09-25 17:47:49 +0900

    refactor: 同じような処理を抽象化しました

--- counter.js
+++ counter.js
@@ -1,46 +1,36 @@
-const $counter = document.getElementById('counter'); 
-const $up = document.getElementById('count-up'); 
-const $down = document.getElementById('count-down'); 
-const $tenUp = document.getElementById('ten-up'); 
-const $tenDown = document.getElementById('ten-down'); 
-const $reset= document.getElementById('reset'); 
+const counter = document.getElementById('counter'); 
+const up = document.getElementById('countUp'); 
+const down = document.getElementById('countDown'); 
+const tenUp = document.getElementById('tenUp'); 
+const tenDown = document.getElementById('tenDown'); 
+const reset = document.getElementById('reset'); 
 let count = 0;
-
-$up.addEventListener('click', function () {
-  count = count + 1;
-  counter.innerHTML = count;
-  string();
-});
-
-$tenUp.addEventListener('click', function () {
-  count = count + 10;
-  counter.innerHTML = count;
-  string();
-});
-
-$down.addEventListener('click', function () {
-  count = count - 1;
-  counter.innerHTML = count;
-  string();
-});
-
-$tenDown.addEventListener('click', function () {
-  count = count - 10;
-  counter.innerHTML = count;
-  string();
-});
-
-$reset.addEventListener('click', function () {
-  count = 0;
-  counter.innerHTML = count;
-  string();
-});
-
 const stringArea = document.getElementById('stringArea');
-const string = () => {
-  if(count == 0){
-    stringArea.innerHTML = "0なんですけど";
-  }else{
-    stringArea.innerHTML = "  ";
-  };
+
+const isShowMessage = () => {
+  return count === 0;
+}
+
+const showMessage = () => {
+  stringArea.innerText = "0なんですけど";
 };
+
+const hideMessage = () => {
+stringArea.innerText = "";
+};
+
+const updateCounter = (num) => {
+  count = count + num;
+  counter.innerText = count;
+  if(isShowMessage()){
+    showMessage();
+  }else{
+    hideMessage();
+  };
+}
+
+up.addEventListener('click', () => updateCounter(1));
+tenUp.addEventListener('click', () => updateCounter(10));
+down.addEventListener('click', () => updateCounter(-1));
+tenDown.addEventListener('click', () => updateCounter(-10));
+reset.addEventListener('click', () => updateCounter(-count)); 
